0f211793748931a47cda83df39d55a0d21efde60
abhaydixit07 <abhaydixit227@gmail.com>
2024-04-10 11:56:10 +0530

    Add passport Auth

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import passport from "passport";
+import { Strategy } from "passport-local";
+import session from "express-session";
 import path from 'path';
 import { ObjectId } from "mongodb";
 import { connectToDb, getDb } from './db.js';
-import { connect } from "mongoose";
-import authRouter from "./routes/auth.js";
 import env from 'dotenv';
-import auth from './middleware/auth.js';
+
+const saltRounds = 10;
 const app = express();
+let db
+const PORT = process.env.PORT || 3000;
+env.config()
+
+app.use(
+    session({
+      secret: "TOPSECRETWORD",
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
 
 // Set up body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(authRouter);
-// Set the view engine to EJS
 app.set('view engine', 'ejs');
-// Serve static files from the 'public' directory
-let db
-const PORT = process.env.PORT || 3000;
-env.config()
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 connectToDb((err)=>{
     if(!err){
@@ -29,50 +41,25 @@ connectToDb((err)=>{
     }
 })
 
-connect(process.env.MONGO_ATLAS_URL)
-  .then(() => {
-    console.log("Connection Successful");
-  })
-  .catch((e) => {
-    console.log(e);
-  });
 
 
-
-
-
-// Route for rendering the index page
-// app.get('/', (req, res) => {
-//     // Supply necessary data to the EJS template
-//     res.render('index.ejs');
-// });
 app.get('/', (req, res) => {
     // Supply necessary data to the EJS template
     res.render('authForm.ejs');
 });
 
-app.post('/uploadpdf', auth, async (req, res) => {
-    try {
-        // Retrieve all filenames from the MongoDB collection, sorted by ObjectId timestamp in descending order
-        const filenames = await db.collection('filenames').find({}).sort({ _id: -1 }).toArray();
-        
-        // Supply necessary data to the EJS template
-        res.render('uploadpdf.ejs', {
-            accessToken: process.env.GITHUB_TOKEN,
-            filenames: filenames,
-            repo: process.env.GITHUB_REPO,
-            owner: process.env.GITHUB_USERNAME
-            
-        });
-    } catch (error) {
-        console.error('Error retrieving filenames:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
+
+app.get("/logout", (req, res) => {
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
 
 
-
-app.post('/uploadfilename', auth, async(req, res) => {
+app.post('/uploadfilename', async(req, res) => {
     // Retrieve the filename from the request body
     const { fileName } = req.body;
     console.log('Received filename:', fileName);
@@ -91,7 +78,8 @@ app.post('/uploadfilename', auth, async(req, res) => {
     console.log(result)
     res.status(200).json({ message: 'Filename received successfully' });
 });
-app.post('/delete', auth, async (req, res) => {
+
+app.post('/delete', async (req, res) => {
     try {
         const fileName = req.body.fileName;
         const result = await db.collection('filenames').deleteOne({ fileName: fileName });
@@ -102,15 +90,64 @@ app.post('/delete', auth, async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
-// app.post('/deleteId', async (req, res) => {
-//     try {
-//         const id = req.body.id;
-//         const result = await db.collection('filenames').deleteOne({ ObjectId : id });
-//         console.log("Delete: ", result)
-//         res.redirect('/uploadpdf')
-//     } catch (error) {
-//         console.error('Error deleting file:', error);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// });
 
+
+app.get("/uploadpdf", async(req, res) => {
+    // console.log(req.user);
+    if (req.isAuthenticated()) {
+        try {
+            // Retrieve all filenames from the MongoDB collection, sorted by ObjectId timestamp in descending order
+            const filenames = await db.collection('filenames').find({}).sort({ _id: -1 }).toArray();
+            
+            // Supply necessary data to the EJS template
+            res.render('uploadpdf.ejs', {
+                accessToken: process.env.GITHUB_TOKEN,
+                filenames: filenames,
+                repo: process.env.GITHUB_REPO,
+                owner: process.env.GITHUB_USERNAME
+                
+            });
+        } catch (error) {
+            console.error('Error retrieving filenames:', error);
+            res.status(500).send('Internal Server Error');
+        }
+    } else {
+      res.redirect("/");
+    }
+  });
+
+  app.post(
+    "/",
+    passport.authenticate("local", {
+      successRedirect: "/uploadpdf",
+      failureRedirect: "/",
+    })
+  );
+  
+passport.use(
+    new Strategy(async function verify(username, password, cb) {
+      try {
+          
+          const storedPassword = process.env.PASSWORD;
+          
+              if (password === storedPassword && username === process.env.USER){
+                //Passed password check
+                return cb(null, username);
+              } else {
+                //Did not pass password check
+                return cb(null, false);
+              }
+
+        
+      } catch (err) {
+        console.log(err);
+      }
+    })
+  );
+  
+  passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
+  passport.deserializeUser((user, cb) => {
+    cb(null, user);
+  });
